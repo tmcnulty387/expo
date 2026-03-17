bb18523080954443b67350f49809bb883a66ae98
tmcnulty387 <tmcnulty387@gmail.com>
2026-03-17 13:55:19 -0400

    Implemented basic local drawing

--- cmd/main.go
+++ cmd/main.go
@@ -18,22 +18,20 @@ import (
 )
 
 var (
-	Red        = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
-	Green      = color.NRGBA{R: 0, G: 255, B: 0, A: 255}
-	Blue       = color.NRGBA{R: 0, G: 0, B: 255, A: 255}
-	Yellow     = color.NRGBA{R: 255, G: 255, B: 0, A: 255}
-	Cyan       = color.NRGBA{R: 0, G: 255, B: 255, A: 255}
-	Magenta    = color.NRGBA{R: 255, G: 0, B: 255, A: 255}
-	Black      = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
-	White      = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
-	Gray       = color.NRGBA{R: 128, G: 128, B: 128, A: 255}
-	Orange     = color.NRGBA{R: 255, G: 165, B: 0, A: 255}
-	tag        = new(int)
-	drawing    = false
-	current    []f32.Point
-	strokes    [][]f32.Point
-	beginCount = 0
-	endCount   = 0
+	Red     = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+	Green   = color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+	Blue    = color.NRGBA{R: 0, G: 0, B: 255, A: 255}
+	Yellow  = color.NRGBA{R: 255, G: 255, B: 0, A: 255}
+	Cyan    = color.NRGBA{R: 0, G: 255, B: 255, A: 255}
+	Magenta = color.NRGBA{R: 255, G: 0, B: 255, A: 255}
+	Black   = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
+	White   = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
+	Gray    = color.NRGBA{R: 128, G: 128, B: 128, A: 255}
+	Orange  = color.NRGBA{R: 255, G: 165, B: 0, A: 255}
+	tag     = new(int)
+	drawing = false
+	current []f32.Point //
+	strokes [][]f32.Point
 )
 
 func main() {
@@ -74,7 +72,7 @@ func draw(ops *op.Ops, source input.Source, size image.Point) {
 	for {
 		ev, ok := source.Event(pointer.Filter{
 			Target: tag,
-			Kinds:  pointer.Press | pointer.Drag | pointer.Release,
+			Kinds:  pointer.Press | pointer.Drag | pointer.Release | pointer.Cancel,
 		})
 		if !ok {
 			break
@@ -82,66 +80,63 @@ func draw(ops *op.Ops, source input.Source, size image.Point) {
 
 		if e, ok := ev.(pointer.Event); ok {
 			switch e.Kind {
-			case pointer.Press | pointer.Drag:
-				if !drawing {
-					current = current[:0]
-					drawing = true
-				}
+			case pointer.Press:
+				drawing = true
+				log.Println("Started Drawing")
 				current = append(current, e.Position)
 			case pointer.Drag:
-				if !drawing {
-					current = current[:0]
-					drawing = true
+				if drawing {
+					current = append(current, e.Position)
 				}
-				current = current[:0]
-				current = append(current, e.Position)
 			case pointer.Release:
-				current = append(current, e.Position)
-				strokes = append(strokes, current)
+				if drawing {
+					current = append(current, e.Position)
+					stroke := make([]f32.Point, len(current))
+					copy(stroke, current)
+					strokes = append(strokes, stroke)
+					current = current[:0]
+					drawing = false
+					log.Println("Stopped Drawing")
+				}
+			case pointer.Cancel:
+				if drawing {
+					stroke := make([]f32.Point, len(current))
+					copy(stroke, current)
+					strokes = append(strokes, stroke)
+					current = current[:0]
+					drawing = false
+					log.Println("Cancelled Drawing")
+				}
 			}
 			log.Println("Event: ", e)
 		}
 	}
-	for _, stroke := range strokes {
+	if drawing {
 		var path clip.Path
 		path.Begin(ops)
-		beginCount++
 
-		path.MoveTo(stroke[0])
-		for _, p := range stroke[1:] {
+		path.MoveTo(current[0])
+		for _, p := range current[1:] {
 			path.LineTo(p)
 		}
-		log.Println("End stroke")
-		path.Close()
-		endCount++
 		paint.FillShape(ops, Black,
 			clip.Stroke{
 				Path:  path.End(),
 				Width: 4,
 			}.Op())
 	}
-	// Draw the button.
-	// var c color.NRGBA
-	// if pressed {
-	// 	c = color.NRGBA{R: 0xFF, A: 0xFF}
-	// } else {
-	// 	c = color.NRGBA{G: 0xFF, A: 0xFF}
-	// }
-	// paint.ColorOp{Color: Black}.Add(ops)
-	// paint.PaintOp{}.Add(ops)
+	for _, stroke := range strokes {
+		var path clip.Path
+		path.Begin(ops)
+
+		path.MoveTo(stroke[0])
+		for _, p := range stroke[1:] {
+			path.LineTo(p)
+		}
+		paint.FillShape(ops, Black,
+			clip.Stroke{
+				Path:  path.End(),
+				Width: 4,
+			}.Op())
+	}
 }
-
-// func strokeTriangle(ops *op.Ops) {
-// var path clip.Path
-// path.Begin(ops)
-// path.MoveTo(f32.Pt(30, 30))
-// path.LineTo(f32.Pt(70, 30))
-// path.LineTo(f32.Pt(50, 70))
-// path.Close()
-
-// 	paint.FillShape(ops, Green,
-// 		clip.Stroke{
-// 			Path:  path.End(),
-// 			Width: 4,
-// 		}.Op())
-// }
