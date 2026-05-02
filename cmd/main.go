82c4cac47212afcd6c303a613a1359ada61fe75b
Sebastian LaVine <mail@smlavine.com>
2026-05-02 11:46:28 -0400

    WIP tls networking

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"flag"
 	"log"
 	"os"
+	"sync"
 
 	"gioui.org/app"
 	"github.com/Go-20255/team-project-malloc4.git/internal/ui"
 )
 
+// TODO: integrate with GUI?
+func networking(ctx context.Context) {
+	conn, err := tls.Dial("tcp", "smlavine.com:443", nil)
+	if err != nil {
+		panic("failed to connect: " + err.Error())
+	}
+	log.Printf("connection: %s\n", conn.RemoteAddr())
+	conn.Close()
+}
 
 func main() {
-	go func() {
-		window := new(app.Window)
-		err := ui.Loop(window)
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
+	var headless bool
+	// TODO server client
+	flag.BoolVar(&headless, "headless", false, "launch without GUI")
+	flag.Parse()
 
-	
-	app.Main()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var networkingTasks sync.WaitGroup
+	networkingTasks.Go(func() { networking(ctx) })
+
+	if headless {
+		networkingTasks.Wait()
+	} else {
+		go func() {
+			window := new(app.Window)
+			err := ui.Loop(window)
+			if err != nil {
+				log.Fatal(err)
+			}
+			cancel()
+			networkingTasks.Wait()
+			os.Exit(0)
+		}()
+		app.Main()
+	}
 }
