62c85fa7a6211ee3eccbb50def71b353d1fa2c18
tmcnulty387 <tmcnulty387@gmail.com>
2026-05-04 00:29:24 -0400

    Implemented JoinSession and CreateSession

--- internal/client/client.go
+++ internal/client/client.go
@@ -9,9 +9,159 @@
 // MessageHeader.Kind.
 package client
 
+import (
+	"context"
+	"encoding/base32"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
+	"github.com/multiformats/go-multiaddr"
+)
+
 // TODO: Command struct that can be used for CLI/GUI to communicate with its
 // Client?
 
 type Client struct {
 	// TODO: fields?
+}
+
+func CreateSession() (string, error) {
+	// start a libp2p node with default settings
+	node, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	// Build AddrInfo
+	peerInfo := peer.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		node.Close()
+		return "", err
+	}
+	log.Printf("Peer Info: %v\n", peerInfo)
+	log.Printf("Peer Addresses: %v\n", addrs)
+
+	// Encode all addresses (let libp2p handle choosing the best one)
+	// Join all multiaddr strings with "|" separator
+	var addrStrings []string
+	for _, addr := range addrs {
+		addrStrings = append(addrStrings, addr.String())
+	}
+	allAddrs := strings.Join(addrStrings, "|")
+
+	// Encode as base32 for the join code
+	joinCode := base32.StdEncoding.EncodeToString([]byte(allAddrs))
+
+	log.Printf("Join Code: %s\n", joinCode)
+	log.Printf("Encoded %d addresses\n", len(addrs))
+
+	// TODO: Keep the node running
+	if err := node.Close(); err != nil {
+		return "", err
+	}
+
+	return joinCode, nil
+}
+
+// JoinSession decodes a base32 join code and connects to the specified peer.
+// Returns the connected libp2p host for further communication.
+func JoinSession(joinCode string) (host.Host, error) {
+	// Decode the base32 join code
+	addrBytes, err := base32.StdEncoding.DecodeString(joinCode)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode join code: %w", err)
+	}
+
+	// Split the addresses by "|" separator
+	allAddrs := string(addrBytes)
+	addrStrings := strings.Split(allAddrs, "|")
+
+	if len(addrStrings) == 0 {
+		return nil, fmt.Errorf("no addresses found in join code")
+	}
+
+	log.Printf("Decoded %d addresses\n", len(addrStrings))
+
+	// Parse all multiaddrs and extract peer info
+	var peerInfo *peer.AddrInfo
+	var addrs []multiaddr.Multiaddr
+
+	for _, addrStr := range addrStrings {
+		addr, err := multiaddr.NewMultiaddr(addrStr)
+		if err != nil {
+			log.Printf("Warning: failed to parse address %s: %v\n", addrStr, err)
+			continue
+		}
+
+		// Extract peer info from the first valid address
+		if peerInfo == nil {
+			info, err := peer.AddrInfoFromP2pAddr(addr)
+			if err != nil {
+				log.Printf("Warning: failed to extract peer info from %s: %v\n", addrStr, err)
+				continue
+			}
+			peerInfo = info
+			addrs = append(addrs, info.Addrs...)
+		} else {
+			// For subsequent addresses, just extract the address part
+			info, err := peer.AddrInfoFromP2pAddr(addr)
+			if err != nil {
+				log.Printf("Warning: failed to extract peer info from %s: %v\n", addrStr, err)
+				continue
+			}
+			addrs = append(addrs, info.Addrs...)
+		}
+	}
+
+	if peerInfo == nil {
+		return nil, fmt.Errorf("failed to extract peer info from any address")
+	}
+
+	// Use all the addresses we collected
+	peerInfo.Addrs = addrs
+
+	log.Printf("Connecting to peer: %s at %v\n", peerInfo.ID, peerInfo.Addrs)
+
+	// Create a new libp2p node
+	node, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create node: %w", err)
+	}
+
+	// Add the peer to the peerstore
+	node.Peerstore().AddAddrs(peerInfo.ID, peerInfo.Addrs, peerstore.PermanentAddrTTL)
+
+	// Connect to the peer
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := node.Connect(ctx, *peerInfo); err != nil {
+		node.Close()
+		return nil, fmt.Errorf("failed to connect to peer: %w", err)
+	}
+
+	// Verify connection
+	if node.Network().Connectedness(peerInfo.ID) != network.Connected {
+		node.Close()
+		return nil, fmt.Errorf("not connected to peer after Connect call")
+	}
+
+	log.Printf("Successfully connected to peer %s\n", peerInfo.ID)
+
+	return node, nil
 }
---
+++ internal/client/client_test.go
@@ -0,0 +1,78 @@
+package client
+
+import (
+	"encoding/base32"
+	"strings"
+	"testing"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
+)
+
+func TestCreateSession(t *testing.T) {
+	joinCode, err := CreateSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if joinCode == "" {
+		t.Fatal("expected non-empty join code")
+	}
+	t.Logf("Generated join code: %s", joinCode)
+}
+
+func TestJoinSession(t *testing.T) {
+	// Create a host that will act as the session host
+	hostNode, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer hostNode.Close()
+
+	// Get the host's addresses
+	peerInfo := peer.AddrInfo{
+		ID:    hostNode.ID(),
+		Addrs: hostNode.Addrs(),
+	}
+	addrs, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Use all addresses (should include localhost for testing)
+	if len(addrs) == 0 {
+		t.Fatal("no addresses found")
+	}
+	t.Logf("Host addresses: %v", addrs)
+
+	// Create a join code from all addresses
+	joinCode := encodeJoinCode(addrs)
+	t.Logf("Join code: %s", joinCode)
+
+	// Now try to join using the join code
+	clientNode, err := JoinSession(joinCode)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer clientNode.Close()
+
+	// Verify connection
+	if clientNode.Network().Connectedness(hostNode.ID()) != network.Connected {
+		t.Fatal("client not connected to host")
+	}
+
+	t.Logf("Successfully connected client %s to host %s", clientNode.ID(), hostNode.ID())
+}
+
+// Helper function to encode multiaddrs as a join code
+func encodeJoinCode(addrs []multiaddr.Multiaddr) string {
+	var addrStrings []string
+	for _, addr := range addrs {
+		addrStrings = append(addrStrings, addr.String())
+	}
+	allAddrs := strings.Join(addrStrings, "|")
+	return base32.StdEncoding.EncodeToString([]byte(allAddrs))
+}
